fd557aadb4d4c103d05f1ae441494fc391906f51
Sergiy Tkach <bees@meta.ua>
2017-12-02 10:58:01 +0200

    Герои в партии могут получать опыт и повышать уровни.

--- Dark Tower/party/party.lua
+++ Dark Tower/party/party.lua
@@ -11,13 +11,15 @@ heroes = {
 		-- Имя
 		name = "Грогнар",
 		-- Класс
-		class= "варвар",
+		class = "warrior",
+		-- Название класса
+		class_name= "варвар",
 		-- Сила
 		strength = 10,
 		-- Ловкость
 		accuracy = 3,
 		-- Телосложение
-		constitution = 3,
+		constitution = 11,
 		-- Интеллект
 		intelligence = 3,
 		-- Здоровье
@@ -28,10 +30,11 @@ heroes = {
 	-- Сир Тавор #2
 	{
 		name = "Сир Тавор",
-		class= "рыцарь",
+		class = "warrior",
+		class_name = "рыцарь",
 		strength = 8,
 		accuracy = 5,
-		constitution = 3,
+		constitution = 10,
 		intelligence = 3,
 		hit_points = 12,
 		mana_points = 5,
@@ -39,7 +42,8 @@ heroes = {
 	-- Вор Хартан #3
 	{
 		name = "Хартан",
-		class= "вор",
+		class = "thief",
+		class_name = "вор",
 		strength = 4,
 		accuracy = 10,
 		constitution = 3,
@@ -50,7 +54,8 @@ heroes = {
 	-- Сорен #4
 	{
 		name = "Сорен",
-		class= "лучник",
+		class = "archer",
+		class_name = "лучник",
 		strength = 6,
 		accuracy = 9,
 		constitution = 3,
@@ -61,21 +66,23 @@ heroes = {
 	-- Фальмиор #5
 	{
 		name = "Фальмиор",
-		class= "волшебник",
+		class = "wizzard",
+		class_name = "волшебник",
 		strength = 3,
 		accuracy = 5,
 		constitution = 3,
-		intelligence = 3,
+		intelligence = 16,
 		hit_points = 7,
 		mana_points = 15,
 	},
 	-- Тилия #6
 	{
 		name = "Тилия",
-		class= "амазонка",
+		class = "warrior",
+		class_name = "амазонка",
 		strength = 7,
 		accuracy = 10,
-		constitution = 3,
+		constitution = 6,
 		intelligence = 3,
 		hit_points = 11,
 		mana_points = 3,
@@ -83,11 +90,12 @@ heroes = {
 	-- Шенна #7
 	{
 		name = "Шенна",
-		class= "колдунья",
+		class = "wizzard",
+		class_name = "колдунья",
 		strength = 4,
 		accuracy = 6,
 		constitution = 3,
-		intelligence = 3,
+		intelligence = 14,
 		hit_points = 9,
 		mana_points = 12,
 	},
@@ -110,13 +118,33 @@ party = {
 	score = 0,
 }
 
+-- Сколько опыта нужно накопить до след. уровня
+function party:next_level_exp(current_level)
+	return (current_level + 1) * 30
+end
+
+-- Добавляем опыт партии
+function party:add_exp(value)
+	if value > 0 then
+		-- Опыт каждому герою партии
+		value = math.ceil(value / party.amount)
+		for i = 1, party.amount do
+			party[i].experience = party[i].experience + value
+			-- Если опыта больше, то новый уровень
+			if party[i].experience > party:next_level_exp(party[i].level) then
+				party[i].level = party[i].level + 1
+			end
+		end
+	end
+end
+
 -- Добавить героя в партию
 -- id - номер героя в массиве heroes
 function party:add_hero(id)
 	-- Проверка на уникальность
 	f = true
 	if party.amount > 0 then
-		for j = 1,party.amount do
+		for j = 1, party.amount do
 			if party[j].name == heroes[id].name then
 				f = false
 				break
@@ -131,12 +159,10 @@ function party:add_hero(id)
 		party[party.amount].id = id
 		-- Имя
 		party[party.amount].name = heroes[id].name
-		-- Уровень
-		party[party.amount].level = 1
-		-- Опыт
-		party[party.amount].experience = 0
 		-- Класс
 		party[party.amount].class = heroes[id].class
+		-- Название класса
+		party[party.amount].class_name = heroes[id].class_name
 		-- Сила
 		party[party.amount].strength = heroes[id].strength
 		-- Ловкость
@@ -151,6 +177,12 @@ function party:add_hero(id)
 		-- Мана
 		party[party.amount].min_mp = heroes[id].mana_points
 		party[party.amount].max_mp = heroes[id].mana_points
+		-- Уровень
+		party[party.amount].level = 1
+		-- Опыт
+		party[party.amount].experience = 0
+		-- Экипировка
+		equipment = {}
 	end
 end
 
@@ -161,5 +193,5 @@ end
 
 print("Состав партии:")
 for i=1,party.amount do
-	print(party[i].name..", "..party[i].class..", уровень "..party[i].level)
+	print(party[i].name..", "..party[i].class_name..", уровень "..party[i].level)
 end
